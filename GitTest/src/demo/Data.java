6df4f8adaa039ada2f412d12d67b459e88f53388
ESSOIYDY <essoiydy@gamil.com>
2017-04-02 09:54:42 +0200

    intial commit Data

---
+++ GitTest/src/demo/Data.java
@@ -0,0 +1,15 @@
+package demo;
+
+public  class Data {
+	static int Data[] = {1,5,10} ;
+
+	public static void display() {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < Data.length; i++) {
+			System.out.println("Data" +i +" : " +Data[i] );
+			
+		}
+		
+	}
+
+}
---
+++ GitTest/src/demo/Main.java
@@ -0,0 +1,20 @@
+package demo ;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	    
+		greeting();
+		Data d = new Data() ;
+		d.display();
+		
+		
+	}
+	
+	private static void greeting() {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO PEOPLE");
+
+	}
+
+}
