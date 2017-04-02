1f91c20813a54769b19caaf3cc0fc04102b957da
ESSOIYDY <essoiydy@gamil.com>
2017-04-02 14:15:17 +0200

    Fix uppercase

--- GitTest/src/demo/Main.java
+++ GitTest/src/demo/Main.java
@@ -13,7 +13,7 @@ public class Main {
 	
 	private static void greeting() {
 		// TODO Auto-generated method stub
-		System.out.println("HELLO PEOPLE");
+		System.out.println("Hello People");
 		System.out.println("Test 1");
 		System.out.println("Test 2");
 
